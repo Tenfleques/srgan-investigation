008457cf3c171c65e12e3b859b378073d7e2592b
Tendai Chikake <tendaichikae@gmail.com>
2019-12-16 02:34:35 +0300

    Updates

---
+++ precache-manifest.0fe649e0d84301b7ded1c4a86904a491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769f57d4d4092ae1ff0f287f21085cb2",
+    "url": "/srgan-investigation/index.html"
+  },
+  {
+    "revision": "476e597fb75684d20583",
+    "url": "/srgan-investigation/static/css/main.7e935d6b.chunk.css"
+  },
+  {
+    "revision": "aad23ef552917cdf47d7",
+    "url": "/srgan-investigation/static/js/2.034fd358.chunk.js"
+  },
+  {
+    "revision": "488f21879f2cc026bc096126e88a8b04",
+    "url": "/srgan-investigation/static/js/2.034fd358.chunk.js.LICENSE"
+  },
+  {
+    "revision": "476e597fb75684d20583",
+    "url": "/srgan-investigation/static/js/main.ce944b37.chunk.js"
+  },
+  {
+    "revision": "ae3e94eabf6308f19f33",
+    "url": "/srgan-investigation/static/js/runtime-main.75e29a06.js"
+  }
+]);
